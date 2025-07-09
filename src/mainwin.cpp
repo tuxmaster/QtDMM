15ff6d587162f1f8fffa09579e9c0ed336fd9c8f
Sarah Mischke <sm.mischke@bally-wulff.de>
2025-07-09 19:23:34 +0200

    autoconnect dmm no need to extra click on the button, you get your readings right away

--- src/mainwin.cpp
+++ src/mainwin.cpp
@@ -22,7 +22,7 @@
 
 #include <QtGui>
 #include <QtWidgets>
-
+#include <QTimer>
 
 #include "mainwin.h"
 #include "mainwid.h"
@@ -86,6 +86,7 @@ MainWin::MainWin(QWidget *parent) : QMainWindow(parent),
     else
       resize(640, 480);
   }
+  QTimer::singleShot(1000, action_Connect, &QAction::trigger);
 }
 
 void MainWin::setConsoleLogging(bool on)
