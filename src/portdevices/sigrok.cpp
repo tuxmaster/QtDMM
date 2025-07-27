b6a434c4f12ebc8d7221fddd670794de99229914
Sarah Mischke <sm.mischke@bally-wulff.de>
2025-07-27 07:44:17 +0200

    port is working, now it needs a decoder

--- src/portdevices/sigrok.cpp
+++ src/portdevices/sigrok.cpp
@@ -4,21 +4,42 @@
 
 
 SigrokDevice::SigrokDevice(const DmmDecoder::DMMInfo &info, QString device, QObject *parent)
-  : QIODevice(parent), m_dmmInfo(info)
+  : QIODevice(parent)
+  , m_dmmInfo(info)
+  , m_process(new QProcess(this))
 {
-
-  auto hp = device.split(':');
+  m_device = device;
+  /*auto hp = device.split(':');
   if (hp.size() == 2)
   {
     m_type   = hp[0];
     m_device = hp[1];
   }
-
+  */
+  connect(m_process, &QProcess::readyReadStandardOutput, this, &SigrokDevice::onProcessReadyRead);
 }
 
 SigrokDevice::~SigrokDevice()
 {
+  close();
 }
+
+
+bool SigrokDevice::init() {
+  if (m_process->state() != QProcess::NotRunning)
+    return false;
+
+  QStringList args;
+  args << "--driver" << m_device << "--continuous";
+
+  m_process->start("sigrok-cli", args);
+  if (!m_process->waitForStarted())
+    return false;
+
+  open(QIODevice::ReadOnly);
+  return true;
+}
+
 
 qint64 SigrokDevice::bytesAvailable() const
 {
@@ -27,7 +48,14 @@ qint64 SigrokDevice::bytesAvailable() const
 
 qint64 SigrokDevice::readData(char *data, qint64 maxSize)
 {
-  return 0;
+  qint64 len = qMin(maxSize, qint64(m_buffer.size()));
+  if (len > 0) {
+    memcpy(data, m_buffer.constData(), len);
+    m_buffer.remove(0, len);
+    QString str = QString::fromUtf8(data, len);
+    qInfo() << str;
+  }
+  return len;
 }
 
 qint64 SigrokDevice::writeData(const char *, qint64)
@@ -37,14 +65,39 @@ qint64 SigrokDevice::writeData(const char *, qint64)
 
 void SigrokDevice::close()
 {
-  if (m_isOpen)
+  if (isOpen())
   {
+    if (m_process->state() != QProcess::NotRunning)
+    {
+      m_process->terminate();
+      if (!m_process->waitForFinished(2000))
+      {
+        m_process->kill();
+        m_process->waitForFinished();
+      }
+    }
+
+    m_buffer.clear();
+    QIODevice::close();
   }
+}
+
+bool SigrokDevice::isOpen() const
+{
+  return m_process && m_process->isOpen() && (m_process->state() == QProcess::Running || m_process->state() == QProcess::Starting);
 }
 
 bool SigrokDevice::availablePorts(QStringList &list)
 {
   // Optional: sigrok scan oder bekannte Geräte
-  list << "uni-t-ut61e-ser:/dev/ttyUSB1";
+  list << "SIGROK uni-t-ut61e-ser:conn=/dev/ttyUSB0";
   return true;
 }
+
+
+void SigrokDevice::onProcessReadyRead() {
+  m_buffer.append(m_process->readAllStandardOutput());
+  Q_EMIT readyRead();
+}
+
+
--- src/portdevices/sigrok.h
+++ src/portdevices/sigrok.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #include <QIODevice>
-
+#include <QProcess>
 #include "dmmdecoder.h"
 
 class SigrokDevice : public QIODevice
@@ -16,21 +16,25 @@ public:
   static bool availablePorts(QStringList &portlist);
 
   void close() override;
-  bool isOpen() const { return m_isOpen; }
+  bool isOpen() const;
+  bool init();
 
   qint64 bytesAvailable() const override;
 
 signals:
   void finished();
 
-protected:
+private slots:
+  void onProcessReadyRead();
+
+private:
   qint64 readData(char *data, qint64 maxSize) override;
   qint64 writeData(const char *data, qint64 len) override;
 
   DmmDecoder::DMMInfo m_dmmInfo;
   bool m_isOpen = false;
   QString m_type, m_device;
-
-
+  QProcess *m_process;
+  QByteArray m_buffer;
 };
 
