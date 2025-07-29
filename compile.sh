d258c56f68946f10180bc195ac0ad780fffd0b89
Sarah Mischke <sm.mischke@bally-wulff.de>
2025-07-29 08:19:29 +0200

    try fix mac build

--- compile.sh
+++ compile.sh
@@ -43,8 +43,8 @@ else
 	CMAKE_PARAMS="-DCMAKE_PREFIX_PATH=$(brew --prefix qt@6)"
 fi
 
-cmake -DBUILD_TESTING=$(${CTEST} && echo "ON" || echo "OFF") -B build
-cmake --build build --parallel ${JOBS} ${CMAKE_PARAMS} || exit 1
+cmake ${JOBS} ${CMAKE_PARAMS} -DBUILD_TESTING=$(${CTEST} && echo "ON" || echo "OFF") -B build
+cmake --build build --parallel  || exit 1
 
 cd build
 
